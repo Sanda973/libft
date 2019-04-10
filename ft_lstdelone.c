217614e0f83c274025de7d60c036d69e2ef4adab
Sandra Gury <sgury@student.42.fr>
2019-04-10 19:23:46 +0200

    beginning bonus ft

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:19:07 by sgury             #+#    #+#             */
-/*   Updated: 2019/04/10 16:21:59 by sgury            ###   ########.fr       */
+/*   Updated: 2019/04/10 19:08:58 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	while (*alst && del
+	if (alst && *alst && del)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		ft_memdel((void **)alst);
+	}
 }
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 13:47:50 by sgury             #+#    #+#             */
-/*   Updated: 2019/04/10 15:39:13 by sgury            ###   ########.fr       */
+/*   Updated: 2019/04/10 19:22:00 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,21 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new_content;
 
-	if ((new_content = (t_list *)malloc(sizeof(t_list)) == NULL))
+	if ((new_content = (t_list *)malloc(sizeof(t_list))) == NULL)
 		return (NULL);
 	if (content == NULL)
 	{
 		new_content->content = NULL;
-		content_size->content = 0;
+		new_content->content_size = 0;
 	}
-	new_content->content = content;
-	new_content->content_size = content_size;
+	else
+	{
+		if ((new_content->content = malloc(content_size)) == NULL)
+			return (NULL);
+		new_content->content = ft_memcpy(new_content->content,
+				content, content_size);
+		new_content->content_size = content_size;
+	}
 	new_content->next = NULL;
 	return (new_content);
 }
